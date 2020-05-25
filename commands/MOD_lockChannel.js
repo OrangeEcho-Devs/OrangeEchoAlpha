67ee3e4dc2ff8837b73a7644057000e7c789d85b
Orange Group <orangemanagementcorpn@gmail.com>
2020-05-25 20:57:49 +0800

    8th edit

--- commands/MOD_lockChannel.js
+++ commands/MOD_lockChannel.js
@@ -14,7 +14,7 @@ module.exports = {
 	const reason = args.join(' ')
   try {
     Community = '685046907961212928'
-    message.guilds.roles.cache.find(role => role.name === Community);
+    const Community = message.guilds.roles.cache.find(role => role.name === "Community")
     channel.updateOverwrite(channel.guild.roles.Community, { SEND_MESSAGES: false });
 		if(args != ''){respond('🔒','<#'+message.channel+'> was locked.\nReason: '+reason, message.channel)}
 		else{respond('🔒','<#'+message.channel+'> was locked. \n', message.channel)}
