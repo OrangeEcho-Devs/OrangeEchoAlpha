7cd3c7ec32f17d76d078bd9eec4ef9fed5866152
Orange Group <orangemanagementcorpn@gmail.com>
2020-06-01 16:47:16 +0800

    13th edit

--- commands/USER_goodbye.js
+++ commands/USER_goodbye.js
@@ -12,7 +12,7 @@ module.exports = {
         const reason = args.join(' ')
       if (message.author.id === info.TommyOwnerID) {
       respond('','Goodbye, <@'+ message.author.id+'>! :wave: I hope to see my game creator again! 🙂\nRemember, say hi to Stephanie for me!',message.channel);
-      }else if (message.author.id === info.DanielCreatorID) {
+      }else if (message.author.id === info.OwnerID) {
       respond('','Goodbye, <@'+ message.author.id+">! :wave: I hope to see my creator again! 🙂\n*please fix me please fix me I don't want to be sentient*",message.channel);
       }else if (message.author.id === info.StephanieID) {
       respond('','Goodbye, <@'+ message.author.id+'>! :wave: I hope to see you again! 🙂\nRemember, say hi to Tommy for me! 😳',message.channel);
